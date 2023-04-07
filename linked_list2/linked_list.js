2a2453ead25dcd50369741cdd7f4b36a24b5c663
Grifo89 <calaca89@gmail.com>
2023-04-06 21:35:14 -0500

    Appliy tsc over ts liked_list implementation

--- linked_list2/linked_list.js
+++ linked_list2/linked_list.js
@@ -49,9 +49,11 @@ var LinkedList = /** @class */ (function () {
             // 6. And because the created node should be the last node now is the next node of the one which was the last node before. 
             lastNode.next = node;
         }
+        ;
         // 7. Returns a node. 
         return node;
     };
+    ;
     // FUNCTION deleteNode: This function delete a node from the list. 
     LinkedList.prototype.deleteNode = function (node) {
         // 1. Checks if the node is the first one, that's mean if it's the head node. if it's the first node assigns to be the head the next node after of the current first node. 
@@ -65,30 +67,43 @@ var LinkedList = /** @class */ (function () {
             prevNode.next = node.next;
         }
     };
+    ;
     // FUNCTION traverse: This function traverses the array. 
     LinkedList.prototype.traverse = function () {
+        // 1. Creates an empty array
         var array = [];
+        // 2. if the list is empty return the empty array
         if (!this.head) {
             return array;
         }
         ;
+        // 3. Creates a helper recursive function that traverses the list from the beginning to the end append the data from each node to the created array until it finishes and returns the array with the list data. 
         var addToArray = function (node) {
             array.push(node.data);
             return node.next ? addToArray(node.next) : array;
         };
+        // 4. Call the helper array passing in the head node as the argument. 
         return addToArray(this.head);
     };
+    ;
+    // FUNCTION size: This function returns the size of the list. 
     LinkedList.prototype.size = function () {
+        // 1. Calls the traverse method to "transform" the list to an array and get the length of that array. 
         return this.traverse().length;
     };
+    // FUNCTION search: This function search a node based on a callback that returns a boolean value. 
     LinkedList.prototype.search = function (comparator) {
+        // 1. Creates a helper recursive function, FUNCTION checkNext which iterates over the list checking the callback condition with each node. 
         var checkNext = function (node) {
+            // 2. Checks if the condition is true for the first node, if it's true returns the first node. 
             if (comparator(node.data)) {
                 return node;
             }
             ;
+            // 3. Iterates over the list recursively checking the callback condition.  
             return node.next ? checkNext(node.next) : null;
         };
+        // 4. If the list is not empty apply the recursive helper function checkNext, else return null. 
         return this.head ? checkNext(this.head) : null;
     };
     return LinkedList;
